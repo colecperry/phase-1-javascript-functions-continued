0af0bdf2a18bead81f56c1ae10da34934f79f759
Cole Perry <colecperry@gmail.com>
2023-02-22 19:38:47 -0800

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+const mondayWork = function (workType = 'go to the office') {
+    return `This Monday, I will ${workType}.`
+}
+
+function wrapAdjective(flareSymbol) {
+    return function(phrase) {
+        const intro = "You are "
+        return intro + flareSymbol + phrase + flareSymbol + "!";
+    }
+}
+
